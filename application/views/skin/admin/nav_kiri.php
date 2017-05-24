4ba1bc0582bb653ab0129fa67482738547b7738b
C-za <sefirman12@gmail.com>
2017-05-24 14:31:25 +0700

    icon navigasi kiri
    DONE

--- application/views/skin/admin/nav_kiri.php
+++ application/views/skin/admin/nav_kiri.php
@@ -11,67 +11,67 @@
 
 						<li class="treeview">
                             <a href="#">
-                                <i class="fa fa-book "></i> <span> Youth Karepmu Control </span>
-                                <i class="fa fa-angle-left pull-right"></i>
+                                <i class="glyphicon glyphicon-asterisk"></i> <span> Youth Karepmu </span>
+                                <i class="glyphicon glyphicon-menu-right pull-right"></i>
                             </a>
                             <ul class="treeview-menu">
-                                <li><a href="add_event.php"><i class="fa fa-angle-double-right"></i> Kelola Posting Karepmu	 </a></li>
-                                <li><a href="event_editors.php"><i class="fa fa-angle-double-right"></i> Kelola Stiker</a></li>
+                                <li><a href="add_event.php"><i class="glyphicon glyphicon-minus"></i> Kelola Posting Karepmu	 </a></li>
+                                <li><a href="event_editors.php"><i class="glyphicon glyphicon-minus"></i> Kelola Stiker</a></li>
                             </ul>
                         </li>
 
 						<li class="treeview">
                             <a href="#">
-                                <i class="fa fa-edit"></i> <span> Youth News Control </span>
-                                <i class="fa fa-angle-left pull-right"></i>
+                                <i class="glyphicon glyphicon-list-alt"></i> <span> Youth News </span>
+                                <i class="glyphicon glyphicon-menu-right pull-right"></i>
                             </a>
                             <ul class="treeview-menu">
-                                <li><a href="add_prestasi.php"><i class="fa fa-angle-double-right"></i> Kelola News </a></li>
-                                <li><a href="prestasi_editor.php"><i class="fa fa-angle-double-right"></i> Validasi News</a></li>
+                                <li><a href="add_prestasi.php"><i class="glyphicon glyphicon-minus"></i> Kelola News </a></li>
+                                <li><a href="prestasi_editor.php"><i class="glyphicon glyphicon-minus"></i> Validasi News</a></li>
                             </ul>
                         </li>
 
                         <li class="treeview">
                             <a href="#">
-                                <i  class="glyphicon glyphicon-shopping-cart"></i> <span> Youth Shopping Control </span>
-                                <i class="fa fa-angle-left pull-right"></i>
+                                <i  class="glyphicon glyphicon-shopping-cart"></i> <span> Youth Shopping </span>
+                                <i class="glyphicon glyphicon-menu-right pull-right"></i>
                             </a>
                             <ul class="treeview-menu">
-                                <li><a href="add_prestasi.php"><i class="fa fa-angle-double-right"></i> Kelola Shopping </a></li>
-                                <li><a href="prestasi_editor.php"><i class="fa fa-angle-double-right"></i> Validasi Shopping </a></li>
+                                <li><a href="add_prestasi.php"><i class="glyphicon glyphicon-minus"></i> Kelola Shopping </a></li>
+                                <li><a href="prestasi_editor.php"><i class="glyphicon glyphicon-minus"></i> Validasi Shopping </a></li>
                             </ul>
                         </li>
 						
                         <li class="treeview">
                             <a href="#">
-                                <i class="fa fa-edit"></i> <span> Youth Wow Control </span>
-                                <i class="fa fa-angle-left pull-right"></i>
+                                <i class="glyphicon glyphicon-star"></i> <span> Youth Wow </span>
+                                <i class="glyphicon glyphicon-menu-right pull-right"></i>
                             </a>
                             <ul class="treeview-menu">
-                                <li><a href="add_prestasi.php"><i class="fa fa-angle-double-right"></i> Kelola Wow </a></li>
+                                <li><a href="add_prestasi.php"><i class="glyphicon glyphicon-minus"></i> Kelola Wow </a></li>
                             </ul>
                         </li>
 
                         <li class="treeview">
                             <a href="#">
-                                <i class="fa fa-edit"></i> <span> Youth Coming Soon Control </span>
-                                <i class="fa fa-angle-left pull-right"></i>
+                                <i class="glyphicon glyphicon-th-list"></i> <span> Youth Coming Soon </span>
+                                <i class="glyphicon glyphicon-menu-right pull-right"></i>
                             </a>
                             <ul class="treeview-menu">
-                                <li><a href="add_prestasi.php"><i class="fa fa-angle-double-right"></i> Kelola Kategori Coming Soon </a></li>
-                                <li><a href="prestasi_editor.php"><i class="fa fa-angle-double-right"></i> Kelola Coming Soon</a></li>
-                                <li><a href="prestasi_editor.php"><i class="fa fa-angle-double-right"></i> Validasi Coming Soon</a></li>
+                                <li><a href="add_prestasi.php"><i class="glyphicon glyphicon-minus"></i> Kelola Kategori </a></li>
+                                <li><a href="prestasi_editor.php"><i class="glyphicon glyphicon-minus"></i> Kelola Coming Soon</a></li>
+                                <li><a href="prestasi_editor.php"><i class="glyphicon glyphicon-minus"></i> Validasi Coming Soon</a></li>
                             </ul>
                         </li>
 
                         <li class="treeview">
                             <a href="#">
-                                <i class="glyphicon glyphicon-collapse-up"></i> <span> Youth Update Control </span>
-                                <i class="fa fa-angle-left pull-right"></i>
+                                <i class="glyphicon glyphicon-collapse-up"></i> <span> Youth Update </span>
+                                <i class="glyphicon glyphicon-menu-right pull-right"></i>
                             </a>
                             <ul class="treeview-menu">
-                                <li><a href="add_prestasi.php"><i class="fa fa-angle-double-right"></i> Kelola Update </a></li>
-                                <li><a href="prestasi_editor.php"><i class="fa fa-angle-double-right"></i> ----</a></li>
+                                <li><a href="add_prestasi.php"><i class="glyphicon glyphicon-minus"></i> Kelola Update </a></li>
+                                <li><a href="prestasi_editor.php"><i class="glyphicon glyphicon-minus"></i> ----</a></li>
                             </ul>
                         </li>
                         

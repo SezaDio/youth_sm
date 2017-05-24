10715ca935c7d4386920127459be72e707119681
C-za <sefirman12@gmail.com>
2017-05-24 23:52:49 +0700

    Start Kelola Shopping
    Baru niat buat data table

---
+++ application/controllers/KelolaShopping.php
@@ -0,0 +1,26 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class KelolaShopping extends CI_Controller {
+
+	public function _construct()
+	{
+		parent::_construct();
+		$this->load->helper('url');
+		$this->load->helper('form');
+		$this->load->library('input');
+		$this->load->library('form_validation');
+		$this->load->library('session');
+
+	}
+
+	public function index()
+	{
+		//$this->load->helper('url');
+		$this->load->view('skin/admin/header_admin');
+		$this->load->view('skin/admin/nav_kiri');
+		$this->load->view('content_admin/kelola_shopping');
+		$this->load->view('skin/admin/footer_admin');
+		
+	}
+}
---
+++ application/views/content_admin/kelola_shopping.php
@@ -0,0 +1,32 @@
+<aside class="right-side">
+                <!-- Content Header (Page header) -->
+                <section class="content-header" style="margin-top: 45px;">
+                    <h1>
+                       Kelola Shopping
+                    </h1>
+                    <ol class="breadcrumb">
+                        <li><a href="#"><i class="fa fa-dashboard"></i>Youth Shopping</a></li>
+                        <li class="active">Kelola Shopping</li>
+                    </ol>
+                </section>
+
+                <!-- Main content -->
+                <section class="content">
+                    <div class="row">
+                        <!-- left column -->
+                        <div class="col-md-12">
+                            <!-- general form elements -->
+                            <div class="box box-primary">
+
+                                    <!--Mulai Tampilkan Data Table-->
+                                    <div class="box-body">
+                                        <div class="form-group">
+                                            
+                                        </div>
+                                    </div><!-- /.box-body -->
+
+                            </div><!-- /.box -->
+                        </div><!--/.col (left) -->
+                    </div>   <!-- /.row -->
+                </section><!-- /.content -->
+            </aside><!-- /.right-side -->

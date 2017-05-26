217f8697c786b82a6b6f527c5fd60f76d28954d2
C-za <sefirman12@gmail.com>
2017-05-26 14:57:25 +0700

    benerin footer admin

--- application/views/skin/admin/footer_admin.php
+++ application/views/skin/admin/footer_admin.php
@@ -20,14 +20,10 @@
                 });
             } );
         </script>
-<<<<<<< HEAD
 
         <!-- Ajax -->
         <script src="<?php echo base_url('asset/js/ajax.js'); ?>" type="text/javascript"></script>
 
-=======
-		<script src="<?php echo base_url('asset/js/ajax.js'); ?>" type="text/javascript"></script>
->>>>>>> origin/master
 		<!-- Morris.js charts -->
         <script src="<?php echo base_url('asset/js/morris/morris.min.js'); ?>" type="text/javascript"></script>
 

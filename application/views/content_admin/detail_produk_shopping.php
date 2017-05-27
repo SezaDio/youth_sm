6156cac12b27f93e7cc91acbb0ec7856d1bc37bc
C-za <sefirman12@gmail.com>
2017-05-27 12:24:26 +0700

    FIX template lihat detail produk

--- application/views/content_admin/detail_produk_shopping.php
+++ application/views/content_admin/detail_produk_shopping.php
@@ -14,25 +14,34 @@
                 <section class="content">
                     <div class="row">
                         <!-- left column -->
-                        <div style="padding-left: 150px">
+                        <div>
+                            <div class="col-md-1">
+                            </div><!--/.col (left) -->
+
                             <div class="col-md-10">
                                 <!-- general form elements -->
                                 <div class="box box-primary">
-                                    
                                     <!--Mulai Tampilkan Data Table-->
                                     <div class="box-body" style="align-content: center;">
-                                            
+                                        <div style="padding-bottom: 10px">
+                                            <div class="col-sm-12">
+                                               <H3 style="text-align: center;"><?php echo $id_produk->nama_produk;?></H3>
+                                               <hr style="outline-color: : steelblue">
+                                            </div>
+                                        </div>
                                             <!--Gambar Produk-->
-                                            <div style="background-color: ghostwhite; border: solid 2px steelblue; height:250px; width: 250px">
-                                                <div class="col-md-6">
-                                                    <label>Gambar</label>
+                                            <div class="col-sm-4" style="padding-top: 20px;">
+                                                <div style="background-color: ghostwhite; border: solid 2px steelblue; height:250px; width: 250px">
+                                                    <div class="col-md-6">
+                                                        <label>Gambar</label>
+                                                    </div>
                                                 </div>
                                             </div>
 
                                             <!--Detail Produk-->
-                                            <div style= "margin-left: 270px; margin-top: -250px;">
-                                                <div style="background-color: ghostwhite; border: solid 2px steelblue; height:250px; width: 495px">
-                                                    <div class="col-md-6">
+                                            <div class="col-sm-8" style="padding-top: 20px;">
+                                                <div style="background-color: ghostwhite; border: solid 2px steelblue; height:250px;">
+                                                    
                                                         <table>
                                                             <tr>
                                                                 <td><label>Detail Produk</label></td>
@@ -40,20 +49,24 @@
                                                                 <td><label><?php echo $id_produk->id_produk;?></label></td>
                                                             </tr>
                                                         </table>
-                                                    </div>
-                                                </div>
+                                                    
+                                                </div>                                              
                                             </div>
-                                            <br><br><br>
-                                            <div style="width: 250px; margin-left: 50px; margin-top: -40px;">
+                                            
+                                            <div style="padding-left: 65px; padding-top: 360px; padding-right: 675px;">
                                                 <!-- Tombol lihat detail -->
                                                 <a href="<?php echo site_url('KelolaShopping');?>"><button class="btn btn-warning btn-sm"><i class="glyphicon glyphicon-arrow-left" ></i> Kembali</button></a>
 
                                                 <!-- Tombol Hapus -->
                                                 <button class="btn btn-danger btn-sm"><i class="glyphicon glyphicon-trash" ></i> Hapus</button>
                                             </div>
+                                            
                                     </div><!-- /.box-body -->
-                                    
+
                                 </div><!-- /.box -->
+                            </div><!--/.col (left) -->
+
+                            <div class="col-md-1">
                             </div><!--/.col (left) -->
                         </div>
                     </div>   <!-- /.row -->

e989f7e340c16fcbaadd060bfb8f727cd956e8ce
C-za <sefirman12@gmail.com>
2017-06-06 23:49:45 +0700

    link logo Administrator

--- application/views/skin/admin/header_admin.php
+++ application/views/skin/admin/header_admin.php
@@ -34,7 +34,7 @@
     <body class="skin-blue">
         <!-- header logo: style can be found in header.less -->
         <header class="header">
-            <a href="#" class="logo">
+            <a href="<?php echo base_url()?>" class="logo">
                 <!-- Add the class icon to your logo image or logo icon to add the margining -->
                 Administrator
             </a>
